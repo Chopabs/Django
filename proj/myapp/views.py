16773a87b304e12631ad1a8b9942ded513704e9f
Pablo <Pabacruz2020@gmail.com>
2025-08-13 16:26:47 +0800

    tapos

--- proj/myapp/views.py
+++ proj/myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect 
 from .models import Room
+from .forms import RoomForm
 # rooms = [
 #     {'id': 1, 'name': 'HTML Basics'},
 #     {'id': 2, 'name': 'CSS Fundamentals'},
@@ -17,3 +18,31 @@ def room(request, pk):
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':    
+        print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form} 
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':    
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form' : form}
+    return render (request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
